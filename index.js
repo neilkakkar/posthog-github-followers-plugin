abbdceb8ad5ed88b07ed2f6d62287b42fc756fef
Neil Kakkar <neilkakkar@gmail.com>
2021-06-16 11:27:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-// Some internal library function
-async function getRandomNumber() {
-    return 4 // remove this line to get an actual random number from random.org – caution, rate limited to 10 events/s!
-    const response = await fetch(
-        'https://www.random.org/integers/?num=1&min=1&max=1000000000&col=1&base=10&format=plain&rnd=new'
-    )
-    return parseInt(await response.text())
-}
+import { RetryError } from '@posthog/plugin-scaffold'
+import fetch from 'node-fetch'
 
-async function setupPlugin({ config }) {
-    console.log(`Setting up the plugin:\n${config.greeting}`)
-}
-
-async function processEvent(event, { config, cache }) {
-    const counterValue = (await cache.get('greeting_counter', 0))
-    cache.set('greeting_counter', counterValue + 1)
-    if (!event.properties) event.properties = {}
-    event.properties['greeting'] = config.greeting
-    event.properties['greeting_counter'] = counterValue
-    event.properties['random_number'] = await getRandomNumber()
-    return event
-}
-
-// The famed Hello World plugin itself
-module.exports = {
-    setupPlugin,
-    processEvent
+/* Runs once every full hour */
+export async function runEveryDay({config}) {
+    const url = `https://api.github.com/users/${config.name}`
+    try {
+        const res = await fetch(url, { method: 'GET' })
+        const response = await res.json()
+        posthog.capture('github_followers', { follower_count: response.followers, username: response.login })
+        console.log('Updated GitHub followers')
+    } catch (err) {
+        console.error('Error getting followers: ', err)
+    }
 }
